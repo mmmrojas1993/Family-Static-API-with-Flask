dd2e4cb47437981bf35eb3996369b90997f69cfa
mmmrojas1993 <lgc.marm@gmail.com>
2022-07-05 16:19:49 +0000

    code realizado

--- src/app.py
+++ src/app.py
@@ -27,16 +27,53 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+# metodo POST
 
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+
+    if not member:
+        return jsonify({"Msj": "Debe enviar un miembro valido!"}), 400
+    jackson_family.add_member(member)
+    return jsonify({"Msj": "Miembro agregado correctamente!"}), 200
+
+# metodo DELETE
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if not member:
+        return jsonify({"Msj": "ID no existe!!"})
+    print("Miembro eliminado!!")
+    return jsonify(member)
+
+
+# Metodo GET por ID
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"Msj": "Miembro no existe!!"}), 400
+
+# metodo UPDATE//PUT
+
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    member = request.json
+    if not member:
+        return jsonify({"Msj": "Debe enviar un miembro valido!"}), 400
+    jackson_family.update_member(member_id, member)
+    return jsonify(member)
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,29 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [ {
+                "id": self._generateId(),
+                "first_name": "hizis",
+                "last_name": self.last_name,
+                "age": 26,
+                "lucky_numbers": [4, 25, 28]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Matteo",
+                "last_name": self.last_name,
+                "age": 1,
+                "lucky_numbers": [25, 28, 4]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Gerardo",
+                "last_name": self.last_name,
+                "age": 21,
+                "lucky_numbers": [28, 4, 25]
+            }
+
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,16 +43,39 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        new_member = {}
+        if "id" in member:
+            new_member["id"] = int(member["id"])
+        else:
+            new_member["id"] = self._generateId()
+
+        new_member["first_name"] = str(member["first_name"])
+        new_member["last_name"] = self.last_name
+        new_member["age"] = int(member["age"])
+        new_member["lucky_numbers"] = member["lucky_numbers"]
+        self._members.append(new_member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == int(id):
+                self._members.pop(position)
+                return {"done": True}
+        return None
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == int(id):
+                return self._members[position]
+        return None
 
     # this method is done, it returns a list with all the family members
+
     def get_all_members(self):
         return self._members
+
+    # UPDATE
+    def update_member(self, id, member):
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == int(id):
+                self._members[position].update(member)
+                return {"done": True}
